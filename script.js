49476122997087791b03d6ebfeea5f8501062754
prince saighe <saigheprince14@gmail.com>
2025-08-02 15:57:42 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    const menuIcon = document.getElementById('menuIcon');
+    const navLinks = document.getElementById('navLinks');
+
+    menuIcon.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+    })
+    
+    function toggleMenu() {
+    document.querySelector('.nav-links').classList.toggle('active');
+};
+
+  window.onscroll = function() {
+    const btn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+      btn.style.display = "block";
+    } else {
+      btn.style.display = "none";
+    }
+  };
+
+  // Scroll to top when clicked
+  document.getElementById("scrollToTopBtn").onclick = function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
